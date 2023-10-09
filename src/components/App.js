e57d1b56d511a95213406cfe1bb0fd8ae8ebc658
Felix Laniyan <flaniyanjr@gmail.com>
2023-10-09 14:47:04 -0400

    tests passed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, {useEffect, useState} from 'react'
+
+function App() {
+    const [dogData, setDogData]= useState({})
+    const [render, setRender]= useState(false)
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r => r.json())
+        .then(dogObj => {
+            setDogData(dogObj)
+            setRender(true)
+        })
+    }, [])
+
+    if (!render) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div>
+            <img src= {dogData.message} alt="A Random Dog"/>
+        </div>
+    )
+}
+
+export default App
